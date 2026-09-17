c92f1dceb11997ef9bf80afea79791b91d92abfb
Raffi38321 <raffi38321@gmai.com>
2026-09-17 10:03:36 +0700

    feat: initial commit

--- Pem-Mobile/Pertemuan-2/ptmn2/App.js
+++ Pem-Mobile/Pertemuan-2/ptmn2/App.js
@@ -1,10 +1,93 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, 
+  Text, 
+  View,
+  Image,
+  Button,
+  ScrollView,
+  FlatList,
+  SectionList,
+  TextInput,
+  TouchableOpacity,
+  Pressable,
+  Switch,
+  Modal,
+  ActivityIndicator,
+  StatusBar,
+  SafeAreaViewBase,
+  Alert,
+  Platform 
+} from 'react-native';
+
+const PROFILE = {
+  name:"Muhammad Raffi",
+  title:"Backend Developer",
+  email:"raffi38321@gmail.com",
+  phone:"0859-7181-5705",
+  location:"Cirebon, West Java",
+  bio:"Lususan bootcamp asah dan coding camp",
+  avatar:"./assets/selena potrait.jpg"
+}
+
+const SKILLS = [
+  {
+    id:"1",name:"ExpressJs", level:90,color:"#844e4e"
+  },
+  {
+    id:"2",name:"NestJs", level:60,color:"#14e2ce"
+  },
+  {
+    id:"3",name:"TypeScipt", level:90,color:"#844e4e"
+  },
+  {
+    id:"4",name:"NextJs", level:80,color:"#45e111"
+  },
+  {
+    id:"5",name:"MongoDB", level:90,color:"#7863c5"
+  },
+  {
+    id:"1",name:"ExpressJs", level:90,color:"#2b064e80"
+  }
+]
+
+const SECTIONS = [
+  {
+    "title":"pengalama kerja",
+    data:[
+      {
+        id:"",
+        role:"",
+        company:"",
+        period:"",
+        desc:""
+      }
+    ]
+  },
+  {
+    "title":"🎓️pedidikan",
+    data :[
+      {
+        id:"e1",
+        role:"S1 informatika",
+        company:"UIN SIBER SYEKH NURJATI CIREBON",
+        periode:"2024-2029",
+        desc:"IPK 3.75 / 4.00 | SKRIPSI Implementasis Machine Learning Pada Web"
+      }
+    ]
+  }
+]
+
+const SOCIAL = [
+  {id:"s1",label:"Github",icon:"🏅",url:"https://github.com/Raffi38321"},
+  {id:"s2",label:"Linkedin",icon:"",url:"www.linkedin.com/in/muhammad-raffi-52658130a"},
+  {id:"s3",label:"Linkedin",icon:"",url:"www.linkedin.com/in/muhammad-raffi-52658130a"},
+]
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{textAlign:"center"}}>Nama Lengkap: Muhammad Raffi</Text>
+      <Text style={{textAlign:"center"}}>{PROFILE.name}</Text>
       <Text style={{textAlign:"center"}}>Tempat Tanggal Lahir:Cirebon 23-januari-2006</Text>
       <Text style={{textAlign:"center"}}>Cita-Cita: Backend Developer</Text>
       <Text style={{textAlign:"center"}}>Rencana Hidup: jadi saya akan terus mengasah skill saya</Text>
---
+++ Pem-Mobile/Pertemuan-3/Laporan-Praktikum.md
@@ -0,0 +1,32 @@
+# Modul Praktikum Pemrograman Mobile Pertemuanv3
+## 🎯 Tujuan Pembelajaran
+
+Setelah menyelesaikan praktikum ini, mahasiswa mampu:
+
+1. Memahami dan menggunakan **16 Core Components** React Native
+2. Menerapkan **StyleSheet** untuk styling terpusat
+3. Menggunakan **useState** untuk state management dasar
+4. Membuat layout yang responsif dengan **Flexbox**
+5. Menangani **interaksi pengguna** (tekan, input, scroll)
+
+LANGKAH 1 - Mengimpor Componenent
+
+1. Buka file App.js pada folder projek ptmn2
+2. import 16 core coomponent sebagai berikut:
+3. Konfirmasi bukti
+    - <img src="image.png" width=50% />
+
+LANGKAH 2 - Menyiapkan data Objek dan Arrays
+1. Membuat Objek untuk menyimpan data profile
+2. Buat Objek Array bernama profile
+3. Konfirmasi Bukti
+    - <img src="image-1.png" width=50% />
+4. Membuat Objek Array Skill untuk menyimpan data SKILLS
+5. Buat Objek skill
+    - <img src="image-2.png" width=50% />
+6.
+7.
+8.
+9.Konfirmasi bukti section
+    - <img src="image-3.png" width=50% />
+1.  KOnfirmasi bukti 
